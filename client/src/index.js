ec1a23fdf444895d21866d757daa6a2bc315d2d4
Ryan Newman <ryannewman2828@gmail.com>
2017-09-28 22:03:02 -0400

    Init Frontend

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Main from './components/Main.jsx';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
-	
-	<BrowserRouter>
-		<Main />
-	</BrowserRouter>, 
-	document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
